0b0d95807170379ff3f2894984c2688feef805fe
pshdev1030 <pshdev1030@gmail.com>
2022-11-16 16:39:26 +0900

    📝update todo

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 
-const useAsync = <T extends unknown>(initialState: T) => {
+const useAsync = <T>(initialState: T) => {
   const [state, setState] = useState<{
     status: "idle" | "pending" | "fulfilled" | "rejected";
     data: T;
@@ -38,5 +38,7 @@ const useAsync = <T extends unknown>(initialState: T) => {
 
   return { data, status, error, execute };
 };
+
+// TODO: useReducer를 사용하는 것이 더 나을수도..?(모든 상태를 하나의 객체로 관리하기 떄문)
 
 export default useAsync;
